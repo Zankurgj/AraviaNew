68dd041762b91b7e927d4989e7aa416aa0abeb82
Ivan Gorbunov <i.gorbunov@corp.101xp.com>
2021-01-19 12:27:04 +0300

    upd

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {});
+document.addEventListener('DOMContentLoaded', function () {
+  rangeSliderInit();
+});
 
 $('#mainSlider').slick({
   slidesToShow: 1,
@@ -104,6 +106,60 @@ $('#socialSlider').slick({
   // ],
 });
 
+$('.services-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  infinite: true,
+  arrows: true,
+  dots: false,
+  prevArrow: $('.slider-arrow--category-prew'),
+  nextArrow: $('.slider-arrow--category-next'),
+  // responsive: [
+  //   {
+  //     breakpoint: 1000,
+  //     settings: {
+  //       dots: false,
+  //       slidesToShow: 1,
+  //       arrows: false,
+  //     },
+  //   },
+  // ],
+});
+
+$('.services-offer-slider').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  infinite: true,
+  arrows: true,
+  dots: false,
+  prevArrow: $('.slider-arrow--offer-prew'),
+  nextArrow: $('.slider-arrow--offer-next'),
+  // responsive: [
+  //   {
+  //     breakpoint: 1000,
+  //     settings: {
+  //       dots: false,
+  //       slidesToShow: 1,
+  //       arrows: false,
+  //     },
+  //   },
+  // ],
+});
+
+$('#productCardSlider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  infinite: true,
+  arrows: false,
+  dots: true,
+  speed: 200,
+  fade: true,
+  customPaging: function (slider, i) {
+    const thumb = $(slider.$slides[i]).data('image');
+    return `<div style="background-image: url('${thumb}');" class="product-slider-pagin"></div>`;
+  },
+});
+
 const onShowTab = (el, id) => {
   const tab = document.querySelector(id);
   const tabName = el.getAttribute('name');
@@ -117,4 +173,62 @@ const onShowTab = (el, id) => {
     $('#productSliderNew').slick('refresh');
     $('#productSliderSpecial').slick('refresh');
   }
+};
+
+const onShowMenuTab = (el, id) => {
+  const tab = document.getElementById(id);
+  const tabName = el.getAttribute('name');
+  const tabsByName = document.querySelectorAll(`[data-tab-name="${tabName}"]`);
+  for (const tabByName of tabsByName) {
+    tabByName.classList.remove('show');
+  }
+  tab.classList.add('show');
+};
+
+const onSpoilerCatagoryToogle = (el) => {
+  el.classList.toggle('opened');
+  $(el).siblings('.category-spoiler-list').slideToggle(200);
+};
+
+const onAddBasket = (el) => {
+  el.classList.toggle('active');
+};
+const onAddFavorites = (el) => {
+  el.classList.toggle('active');
+};
+const rangeSliderInit = () => {
+  $('.catalog-range').slider({
+    range: true,
+    min: 0,
+    max: 100000,
+    values: [550, 17999],
+    slide: function (event, ui) {
+      $('#firstVal').html(ui.values[0]);
+      $('#nextVal').html(ui.values[1]);
+    },
+  });
+  $('#firstVal').html($('.catalog-range').slider('values', 0));
+  $('#nextVal').html($('.catalog-range').slider('values', 1));
+};
+
+const onCounterMinus = (id) => {
+  let val = getCounterVal(id);
+  if (val > 0) {
+    --val;
+  }
+  setCounterVal(id, val);
+};
+
+const onCounterPlus = (id) => {
+  let val = getCounterVal(id);
+  ++val;
+  setCounterVal(id, val);
+};
+
+const getCounterVal = (id) => {
+  return document.getElementById(id).value;
+};
+
+const setCounterVal = (id, val) => {
+  document.getElementById(id).value = val;
 };
--- gulpfile.js
+++ gulpfile.js
@@ -61,6 +61,8 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.min.js',
     'node_modules/slick-carousel/slick/slick.min.js',
+    'node_modules/jquery-ui-dist/jquery-ui.min.js',
+    'node_modules/slick-carousel/slick/slick.min.js',
     'app/js/app.js', // app.js. Always at the end
   ])
     .pipe(concat('app.min.js'))
